2be11b34f48890bb582c974febcdf46d7fa0b9b0
siyeol97 <leesi2830@naver.com>
2024-05-31 13:15:39 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <span>Hello World!</span>
+      <span>Next.js + Zustand + React-query boilerplate</span>
+    </main>
+  );
+}
